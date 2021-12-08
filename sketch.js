f6e85e293d502ec13c9a93f08b3f4c9235408b3f
Greg Kreisman <gkreisman_cep@qq.com>
2021-12-08 20:38:22 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let sbtm;
+let sp;
+let lns =80;
+let h;
+
+function setup(){
+  createCanvas(800,800);
+  //fullScreen();
+  background(0);
+  stroke(0,255,0);
+  sp= width/lns;
+  
+  sbtm = 10;
+  h=0;
+  
+}
+
+function draw(){
+  
+  background(0);
+  h= map(mouseY,0,height,0,height-height/4);
+  sbtm = map(mouseX,0,width,10,60);
+  noFill();
+  ellipse(width-width/4,h-height/4,height/5,height/5);
+  rect((frameCount*2)%width,h-height/9,height/5,height/20);
+  for(let i=0;i<lns;i++){
+    line(i*sp,h,(i-lns/2)*sp*sbtm,height);
+  }
+}
